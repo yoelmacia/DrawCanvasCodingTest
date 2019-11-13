e3270c82867a21d92fccc98ad06ae5a7f9c2e6e2
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-11-13 11:47:50 +0100

    Add canvas component
    Fetch the data to the component with axios,
    visualice the data, get rid of the console
    errors from the ESLint.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Canvas></Canvas>
+  </div>
 </template>
 
 <script>
+import Canvas from "./components/Canvas.vue";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    Canvas
+  }
 };
 </script>
 
---
+++ src/components/Canvas.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="fetchData()"></button>
+    {{ dataArray }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Canvas",
+  data() {
+    return {
+      dataArray: [],
+      url: "https://about-you-pangea.s3.eu-central-1.amazonaws.com/shapes.json"
+    };
+  },
+  methods: {
+    fetchData() {
+      axios(this.url)
+        .then(response => {
+          this.dataArray = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style></style>
